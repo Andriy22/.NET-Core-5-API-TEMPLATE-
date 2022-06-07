93a81c50927268551b01ccd0ec56fe951a7c5509
istep.andriy <istep.andriy@gmail.com>
2022-06-07 20:27:25 +0300

    Add project files.

---
+++ Application/Interfaces/IDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IDBContext
+    {
+        DbSet<Domain.Demo> Demos { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Persistence/DBContext.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Persistence.EntityTypeConfigurations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+
+    public class DBContext : DbContext, IDBContext
+    {
+        public DbSet<Demo> Demos { get; set; }
+
+        public DBContext(DbContextOptions options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new DemoConfiguration());
+            base.OnModelCreating(builder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
